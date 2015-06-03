4b2acbe0d139e6a031de98db09c87ce4a6872dbf
NataliaLKB <natalialeebaltazar@gmail.com>
2015-06-03 12:46:31 +0100

    making src/app.jsx to compile to public/js/app.js

---
+++ src/components/container.jsx
@@ -0,0 +1,24 @@
+var React = require("react");
+// var Container = require("./container.jsx");
+
+
+var Container = React.createClass({
+	getInitialState: function() {
+		return {
+
+		}
+	},
+
+	componentDidMount: function() {
+
+	},
+	
+	render: function() {
+		return (
+			<h1>This is working...</h1>
+		);
+	}
+
+});
+
+module.exports = Container;
